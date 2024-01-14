23991186e0a75b94c23c622f27f1892fc042d75b
Duc Thai <ducthai060501@gmail.com>
2024-01-14 15:05:48 +0200

    some functionality to read resume and group text into lines

---
+++ app/lib/parse-resume-from-pdf/index.ts
@@ -0,0 +1,13 @@
+import { groupTextItemsIntoLines } from "./group-text-items-into-lines";
+import { readPdf } from "./read-pdf";
+
+export const parseResumeFromPdf = async (fileUrl: string) => {
+
+    // step 1: reada pdf resume file into text items to prepare for processing
+    const textItems = await readPdf(fileUrl);
+
+    // step 2: Group text items into line
+    const lines = groupTextItemsIntoLines(textItems);
+
+    // step 3: Group lines into sections
+}
